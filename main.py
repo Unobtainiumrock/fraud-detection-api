125f2bbf6ecf4d43e457acb09ccf3b7401378eb2
Nicholas Fleichhauer <unobtainiumrock@gmail.com>
2024-09-12 03:13:45 -0700

    Add more packages to the requirements.txt, hash out more play-code with FAST API, explore how I want the project structre to be set up.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 import joblib
 import numpy as np
+import databases
 
 app = FastAPI()
 
@@ -13,16 +14,57 @@ class Transaction(BaseModel):
     account_id: int
     location: str
 
-# model = joblib.load('path_to_model.pkl')
+random_cut_forest_model = joblib.load('./trained_models/random_cut_forest_model.pkl')
+xgboost_model = joblib.load('./trained_models/xgboost_model.pkl')
+
+################### Routes ###################
+@app.on_event('startup')
+async def startup():
+    await database.connect()
+
+@app.on_event('shutdown'):
+    await database.disconnect()
 
 @app.post('/predict')
-async def predict(transaction: Transaction):
-    features = np.array([[transaction.amount, transaction.time, transaction.type, transaction.account_id, transaction.location]])
-    # prediction = model.predict([features])
-    # return {'prediction': int(prediction[0])}
-    return { "hello": "world!" }
+async def predict(transaction: Transaction, model):
+    try:
+        features = np.array([[
+            transaction.amount,
+            transaction.time,
+            transaction.type,
+            transaction.account_id,
+            transaction.location
+        ]])
+        prediction = model.predict(features)
 
-# Some other thigns to note. We'd want to ensure that the outputs (e.g. fraud probability) can 
-# be easily accessed and utilized by other systems within Coinbase. This could involve
-# - Storing predictions in a database for further analysis
-# - Triggering alerts or workflows based on certain prediction thresholds.
+
+        return {'prediction': int(prediction[0])}
+        # return { "hello": "world!" }
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
+
+##############################################
+
+
+# This system needs to be real-time, meaning that we want to have things like:
+
+# prediction_value = int(prediction[0])
+# if prediction_value > THRESHOLD:
+#     # Trigger an alert (could be an email, a webhook call, etc.)
+#     trigger_alert(prediction_value)
+# return {'prediction': prediction_value}
+
+# We may also want data persistence with stuff like:
+
+# prediction = model.predict(features)
+# query = "INSERT INTO predictions (prediction) VALUES (:prediction)"
+# await database.execute(query, {"prediction": int(prediction[0])})
+
+
+# Keep the system as decoupled as possible and make routes and functions have single
+# responsibilities!
+
+# Add a preprocessing pipeline and feature engineering pipe line (if needed) borrow the
+# reusable pipeline framework from my previous ML work.
+
