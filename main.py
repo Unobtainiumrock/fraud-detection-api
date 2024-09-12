4ebe09a1023e096638d537f33a8a0117f26b3c33
Nicholas Fleichhauer <unobtainiumrock@gmail.com>
2024-09-11 18:55:12 -0700

    temp change to see if api docs will generate

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ class Transaction(BaseModel):
     account_id: int
     location: str
 
-model = joblib.load('path_to_model.pkl')
+# model = joblib.load('path_to_model.pkl')
 
 @app.post('/predict')
 async def predict(transaction: Transaction):
     features = np.array([[transaction.amount, transaction.time, transaction.type, transaction.account_id, transaction.location]])
-    prediction = model.predict([features])
-    return {'prediction': int(prediction[0])}
+    # prediction = model.predict([features])
+    # return {'prediction': int(prediction[0])}
+    return "Hello world!"
 
 # Some other thigns to note. We'd want to ensure that the outputs (e.g. fraud probability) can 
 # be easily accessed and utilized by other systems within Coinbase. This could involve
